17ec97e6baef221a542abc8e771042f8423f1c8c
Dean Byrne <deanbyrne95@gmail.com>
2019-08-06 11:56:26 +0100

    update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+    name = 'Dean Byrne';
+    title = 'Portfolio';
+
+    constructor(private titleService: Title) { }
+
+    ngOnInit() {
+        this.titleService.setTitle(this.name + ' | ' + this.title);
+    }
 }
