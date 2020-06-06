5d20229fbb4b491e9c76a076be9b09534be5dfeb
deanbyrne95 <deanbyrne95@gmail.com>
2020-06-06 19:00:07 +0100

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './core/components/home/home.component';
+import { AboutComponent } from './core/components/about/about.component';
+import { WorkComponent } from './core/components/about/work/work.component';
+import { EducationComponent } from './core/components/about/education/education.component';
+import { PageNotFoundComponent } from './core/components/error/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', children: [
+    { path: '', component: AboutComponent },
+    { path: 'education', component: EducationComponent },
+    { path: 'work', component: WorkComponent }
+    ]
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'education', redirectTo: '/about/education', pathMatch: 'full' },
+  { path: 'work', redirectTo: '/about/work', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
