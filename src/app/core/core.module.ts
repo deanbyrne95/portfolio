42740ea8b4bab8816ede32dd206f97e9711b2e7b
deanbyrne95 <deanbyrne95@gmail.com>
2020-06-06 18:59:54 +0100

    update core module, add import module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from './components/home/home.component';
+import { WorkComponent } from './components/about/work/work.component';
+import { EducationComponent } from './components/about/education/education.component';
+import { AboutComponent } from './components/about/about.component';
+import { PageNotFoundComponent } from './components/error/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, WorkComponent, EducationComponent, AboutComponent, PageNotFoundComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule
+  ],
+  exports: [HomeComponent, WorkComponent, EducationComponent]
 })
 export class CoreModule { }
